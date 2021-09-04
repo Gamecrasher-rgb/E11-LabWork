24c20be400722397ba7956a9ae500b57cd0dc9a5
Ali Hanks <alihanks@gmail.com>
2021-09-03 23:59:35 -0700

    Added AirQuality Code

---
+++ AirQualityDAQ.py
@@ -0,0 +1,11 @@
+#Air Quality Script
+import serial
+port = serial.Serial("/dev/serial0", baudrate=9600, timeout=1.5)
+
+text = port.read(32)
+
+pm1 = int.from_bytes(text[3:5], byteorder='big')
+pm2 = int.from_bytes(text[5:7], byteorder='big')
+pm3 = int.from_bytes(text[7:9], byteorder='big')
+
+pm4 = int.from_bytes(text[17:19], byteorder='big')
