00ef6daebec8c6ef79c90ba1cf19b09362705895
Gilder <gildermontepeque52@gmail.com>
2021-09-30 19:04:04 -0700

    Fixed Sleep_time bug

--- WeatherDAQ.py
+++ WeatherDAQ.py
@@ -41,7 +41,6 @@ start_time = time.time()
 stop_time = start_time + run_time
 current_time = time.time()
 
-sleep_time = float(input("How long should the sleep be between each data grab: "))
 
 while current_time < run_time + start_time: 
 	
