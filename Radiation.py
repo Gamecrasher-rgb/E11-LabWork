d16fb34156de9ea8ae97cebb5b251a0285ab4d19
DRBerkeleyGitHub <90800333+DRBerkeleyGitHub@users.noreply.github.com>
2021-10-20 13:41:05 -0700

    Added testable internet code

--- Radiation.py
+++ Radiation.py
@@ -1,3 +1,33 @@
 import time
 import datetime
 import RPI.GPIO as GPIO
+
+def Int21R( channel ):
+  global Count
+
+  Count += 1
+  GPIO.output( 20, GPIO.HIGH )
+  time.sleep( 0.000001 )
+  GPIO.output( 20, GPIO.LOW )
+
+
+GPIO.setwarnings(False)
+GPIO.setmode( GPIO.BCM )
+
+GPIO.setup( 20, GPIO.OUT )
+GPIO.setup( 21, GPIO.IN, pull_up_down = GPIO.PUD_OFF )
+GPIO.add_event_detect( 21, GPIO.RISING, callback = Int21R )
+
+Count = 0
+while True:
+  Time = datetime.datetime.now()
+  print( "%s - Count = %6d  RPM = %6d" % ( Time, Count, Count * 60.0 ) )
+  RPM = Count * 60.0
+  Count = 0
+  t = time.time( )
+  Wait = 1 - ( t - int( t ) )
+  time.sleep( Wait )
+except KeyboardInterrupt:
+  GPIO.cleanup( )
+
+GPIO.cleanup( )
