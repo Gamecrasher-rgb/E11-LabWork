ddfa67339b5b47918f11ef0ec2cb681e60a35eee
DRBerkeleyGitHub <90800333+DRBerkeleyGitHub@users.noreply.github.com>
2021-10-20 14:18:28 -0700

    Update RadWatchTest.py

--- RadWatchTest.py
+++ RadWatchTest.py
@@ -12,9 +12,9 @@ GPIO.setup(25, GPIO.IN)    # set GPIO25 as input (button)
 # Define a threaded callback function to run in another thread when events are detected  
 def my_callback(channel):  
     if GPIO.input(25):     # if port 25 == 1  
-        print "Rising edge detected on 25"  
+        print ("Rising edge detected on 25")  
     else:                  # if port 25 != 1  
-        print "Falling edge detected on 25"  
+        print ("Falling edge detected on 25")  
   
 # when a changing edge is detected on port 25, regardless of whatever   
 # else is happening in the program, the function my_callback will be run  
