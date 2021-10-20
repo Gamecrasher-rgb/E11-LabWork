bf8c8bda7e54b2c1521a21eef5f4dd63db7f9f03
Gilder <gildermontepeque52@gmail.com>
2021-10-20 15:02:22 -0700

    Update GoodRadWatch.py

--- GoodRadWatch.py
+++ GoodRadWatch.py
@@ -10,7 +10,7 @@ def callBack(channel):
     global counts
     if GPIO.input(17):     # if port 17 == 1  
         print ("Count Detected!") 
-        counts =+ 1 
+        counts = counts + 1
 
 GPIO.add_event_detect(17, GPIO.BOTH, callback=callBack)  
     
