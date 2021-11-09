a6bf3b2806e0be3717b29cc88ab3d0a7472f46bd
Gilder <gildermontepeque52@gmail.com>
2021-11-09 15:06:52 -0800

    Update GPS_test_code.py

--- GPS_test_code.py
+++ GPS_test_code.py
@@ -1,67 +1,22 @@
-from gps import *
-import time
-import threading
-import math
-import sys
+import gps
 
-class GpsController(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-        self.running = False
-   
-    def run(self):
-        self.running = True
-        while self.running:
-            # grab EACH set of gpsd info to clear the buffer
-            self.gpsd.next()
+# Listen on port 2947 (gpsd) of localhost
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-    def stopController(self):
-        self.running = False
- 
-    @property
-    def fix(self):
-        return self.gpsd.fix
-
-    @property
-    def utc(self):
-        return self.gpsd.utc
-
-    @property
-    def satellites(self):
-        return self.gpsd.satellites
-
-if __name__ == '__main__':
-    # create the controller
-    gpsc = GpsController() 
+while True:
     try:
-        # start controller
-        gpsc.start()
-        while True:
-            print ("latitude ", gpsc.fix.latitude)
-            print ("longitude ", gpsc.fix.longitude)
-            print ("time utc ", gpsc.utc, " + ", gpsc.fix.time)
-            print ("altitude (m)", gpsc.fix.altitude)
-            print ("eps ", gpsc.fix.eps)
-            print ("epx ", gpsc.fix.epx)
-            print ("epv ", gpsc.fix.epv)
-            print ("ept ", gpsc.gpsd.fix.ept)
-            print ("speed (m/s) ", gpsc.fix.speed)
-            print ("climb ", gpsc.fix.climb)
-            print ("track ", gpsc.fix.track)
-            print ("mode ", gpsc.fix.mode)
-            print ("sats ", gpsc.satellites)
-            time.sleep(0.5)
-
-    #Error
-    except:
-        print ("Unexpected error:", sys.exc_info()[0])
-        raise
-
-    finally:
-        print ("Stopping gps controller")
-        gpsc.stopController()
-        #wait for the tread to finish
-        gpsc.join()
-     
-    print ("Done")
+        report = session.next()
+        # Wait for a 'TPV' report and display the current time
+        # To see all report data, uncomment the line below
+        # print(report)
+        if report['class'] == 'TPV':
+            if hasattr(report, 'time'):
+                print(report.time)
+    except KeyError:
+        pass
+    except KeyboardInterrupt:
+        quit()
+    except StopIteration:
+        session = None
+        print("GPSD has terminated")
