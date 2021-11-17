91eb349379d1581d40733a7ce2c38d95bb4bca96
Gilder <gildermontepeque52@gmail.com>
2021-11-17 12:22:00 -0800

    Update GPS_test_code.py

--- GPS_test_code.py
+++ GPS_test_code.py
@@ -2,10 +2,10 @@ import gps
 import os
 
 # Listen on port 2947 (gpsd) of localhost
-os.system(r"sudo killall gpsd")
-os.system(r"sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock")
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+os.system(r"sudo killall gpsd")
+os.system(r"sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock")
 menu = int(input("1. Print Latitude, Longitude, and Time\n2. Menu Screen\n"))
 print("\n")
 while True:
